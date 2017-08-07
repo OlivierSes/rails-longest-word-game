dfea9a878691206fd8b54b38d6a1659981ab5cd9
Olivier Sesboue <sesboue.olivier@gmail.com>
2017-08-07 17:34:11 +0200

    my first web application

---
+++ app/controllers/game_controller.rb
@@ -0,0 +1,69 @@
+require 'open-uri'
+require 'json'
+require 'date'
+
+class GameController < ApplicationController
+
+  def game_welcome
+  end
+
+  def launch_game
+    @grid_size = params[:size].to_i
+    @grid = generate_grid(@grid_size)
+    @start_time = params[:time]
+  end
+
+  def generate_grid(grid_size)
+    grid = []
+    n = grid_size / 2
+    n.times do
+      grid.push(["A", "E", "I", "O", "U", "Y"].sample)
+    end
+    consol_grid = ("A".."Z").to_a.reject { |voyel| ["A", "E", "I", "O", "U", "Y"].include?(voyel) }
+    (grid_size - n).times do
+      grid.push(consol_grid.to_a.sample)
+    end
+    return grid.shuffle
+  end
+
+  def score
+    @attempt = params[:answer]
+    @start_time = Time.parse(params[:start_time])
+    @end_time = Time.now
+    @grid= params[:grid].split("")
+    result = run_game(@attempt, @grid, @start_time, @end_time)
+    @time = result[:time].round(1)
+    @score = result[:score].round(2)
+    @message = result[:message]
+  end
+
+  def run_game(attempt, grid, start_time, end_time)
+    time = end_time - start_time
+    result_api = { time: time, translation: " ", score: 0, message: "not in the grid" }
+    return result_api if grid_include?(attempt, grid) == false
+    url = "https://wagon-dictionary.herokuapp.com/#{attempt.downcase}"
+    result_api = JSON.parse(open(url).read)
+    return result(result_api, time)
+  end
+
+  def result(result_api, time)
+    result = { time: time, translation: " ", score: 0, message: "not an english word" }
+    return result if result_api["found"] == false
+    result[:score] = result_api["length"] - time / 10
+    result[:message] = "Well done"
+    return result
+  end
+
+  def grid_include?(attempt, grid)
+    attempt.upcase.split("").each do |item|
+      if grid.include?(item)
+        grid.delete_at(grid.index(item))
+      else
+        return false
+      end
+    end
+    return true
+  end
+
+
+end
